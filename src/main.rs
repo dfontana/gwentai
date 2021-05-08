1197a962aabc089a164b596c1137f7b03a01c9f8
Dylan Fontana <dyfontana@gmail.com>
2021-05-08 16:04:28 -0400

    Implemented bar

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,39 @@
 mod deck;
 mod disk;
+mod progress;
 
 use anyhow::Error;
 use deck::{Deck, DeckListResponse, DeckResponse};
 use disk::Disk;
 use futures::{future, stream, StreamExt};
+use progress::Progress;
 use reqwest::Client;
 
 const BASE_API: &'static str = "https://www.playgwent.com/en/decks/api/guides";
 const CONCURRENT_REQUESTS: usize = 30;
 const PATH: &'static str = "decks.json";
 
-async fn get_deck_page(http: &Client, page: usize) -> Result<(usize, DeckListResponse), Error> {
+async fn get_deck_page(
+  http: &Client,
+  progress: &Progress,
+  page: usize,
+) -> Result<(usize, DeckListResponse), Error> {
   let url = format!("{}/offset/{}/limit/500", BASE_API, page * 500);
-  println!("{}", &url);
-  Ok((
-    page,
-    http
-      .get(url)
-      .send()
-      .await?
-      .json::<DeckListResponse>()
-      .await?,
-  ))
+  let res = http.get(url).send().await?;
+  let json = res.json::<DeckListResponse>().await?;
+  progress.increment(1);
+  Ok((page, json))
 }
 
-async fn get_deck(http: &Client, id: usize) -> Result<(usize, DeckResponse), Error> {
+async fn get_deck(
+  http: &Client,
+  progress: &Progress,
+  id: usize,
+) -> Result<(usize, DeckResponse), Error> {
   let url = format!("{}/{}", BASE_API, id);
-  println!("{}", &url);
-  Ok((
-    id,
-    http.get(url).send().await?.json::<DeckResponse>().await?,
-  ))
+  let res = http.get(url).send().await?.json::<DeckResponse>().await?;
+  progress.increment(1);
+  Ok((id, res))
 }
 
 #[tokio::main]
@@ -39,11 +41,9 @@ async fn download(client: &Client) -> Disk {
   // TODO right now the upper bound on pages is 60, but you'll want a way to incrementally
   //      increase this each time this is ran.
   let range = 0..60;
-
-  println!("Items to download: {}", range.len());
-
+  let meta_progress = Progress::new("DL Metadata", range.len());
   let disk: Disk = stream::iter(range)
-    .map(|page| get_deck_page(&client, page))
+    .map(|page| get_deck_page(&client, &meta_progress, page))
     .buffer_unordered(CONCURRENT_REQUESTS)
     .filter_map(|res| {
       match res {
@@ -61,10 +61,9 @@ async fn download(client: &Client) -> Disk {
     })
     .await;
 
-  println!("Items to download: {}", disk.deckmeta.len());
-
+  let deck_progress = Progress::new("DL Decks", disk.deckmeta.len());
   let decks = stream::iter(disk.deckmeta.keys().into_iter())
-    .map(|id| get_deck(&client, id.to_owned()))
+    .map(|id| get_deck(&client, &deck_progress, id.to_owned()))
     .buffer_unordered(CONCURRENT_REQUESTS)
     .filter_map(|res| match res {
       Ok((pg, d)) => future::ready(Some((pg, d.deck))),
